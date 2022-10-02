bf62471df7a1090135c556a2ab745e3f11314d9d
DONGKYU NA <kaineus86@gmail.com>
2022-10-02 16:44:55 +0900

    최초 커밋

---
+++ src/test/java/com/pluxity/pxcore/PxCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluxity.pxcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PxCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
